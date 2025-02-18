89a9889cc3c7851391019e849c3c13e0e1b7c1c5
oVirtualRealityo <148754947+oVirtualRealityo@users.noreply.github.com>
2025-02-18 13:51:53 +0100

    Update index.tsx

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -99,7 +99,7 @@ const postPage = ({ posts }: postPageProps) => {
       <h1 className={styles.pageTitle}>Mijn Blogs</h1>
      
       {admin && (
-        <Addform/>
+        <AddForm/>
 )}
       <BlogHeader filter={filter} setFilter={setFilter} handleAdmin={handleAdmin} admin={admin}/>
     
